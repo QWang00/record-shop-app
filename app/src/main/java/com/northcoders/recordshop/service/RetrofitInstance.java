0af1aa09a53c771de72daf394631ae5811e021b5
qiuqiu <qiujuanwang87@gmail.com>
2024-09-18 12:10:53 +0100

    feat: configure RetrofitInstance for API requests in RetrofitInstance class
    - Set up Retrofit with GsonConverterFactory for JSON parsing.
    - Added HttpLoggingInterceptor for logging HTTP requests and responses.
    - Updated BASE_URL with API endpoint URL.

---
+++ app/src/main/java/com/northcoders/recordshop/service/RetrofitInstance.java
@@ -0,0 +1,26 @@
+package com.northcoders.recordshop.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static Retrofit retrofit = null;
+    private final static String BASE_URL = "RecordShop-env.eba-qatztzb9.eu-west-2.elasticbeanstalk.com/albums/";
+
+    public static AlbumApiService getService(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+
+        }
+        return retrofit.create(AlbumApiService.class);
+    }
+}
