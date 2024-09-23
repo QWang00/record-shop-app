2fd95a85a67d2b605578ace22fe822b95562fcc3
qiuqiu <qiujuanwang87@gmail.com>
2024-09-23 12:08:43 +0100

    feat: implement item click listener for RecyclerView items in AlbumAdapter

--- app/src/main/java/com/northcoders/recordshop/ui/mainactivity/AlbumAdapter.java
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/AlbumAdapter.java
@@ -1,44 +1,50 @@
 package com.northcoders.recordshop.ui.mainactivity;
 
+import static android.os.Build.VERSION_CODES.R;
+
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
 import com.northcoders.recordshop.R;
 import com.northcoders.recordshop.databinding.AlbumItemBinding;
 import com.northcoders.recordshop.model.Album;
-import java.util.ArrayList;
-
-
+import com.northcoders.recordshop.repository.AlbumImageRepository;
 
 
 public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder> {
     private Context context;
-    private ArrayList<Album> albumList;
+    private List<Album> albumList;
+    private AlbumImageRepository albumImageRepository;
+    private final RecyclerViewInterface recyclerViewInterface;
 
-    public AlbumAdapter(ArrayList<Album> albumList, Context context) {
+
+    public AlbumAdapter(Context context, List<Album> albumList, RecyclerViewInterface recyclerViewInterface) {
         this.albumList = albumList;
-
+        this.recyclerViewInterface = recyclerViewInterface;
+        albumImageRepository = new AlbumImageRepository();
     }
+
     @NonNull
     @Override
     public AlbumViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         AlbumItemBinding binding = DataBindingUtil
                 .inflate(
-                        LayoutInflater.from(parent.getContext()),
-                        R.layout.album_item,
-                        parent,
-                        false);
-        return new AlbumViewHolder(binding);
+                        LayoutInflater.from(parent.getContext()), com.northcoders.recordshop.R.layout.album_item,
+        parent, false);
+        return new AlbumViewHolder(binding, recyclerViewInterface);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
         Album album = albumList.get(position);
+        albumImageRepository.fetchAlbumImage((album.getArtist() + " " + album.getName()), holder.albumItemBinding.imgViewAlbum);
         holder.albumItemBinding.setAlbum(album);
     }
 
@@ -47,12 +53,27 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
         return albumList.size();
     }
 
-    public static class AlbumViewHolder extends RecyclerView.ViewHolder {
+
+    public static class AlbumViewHolder extends RecyclerView.ViewHolder{
         private AlbumItemBinding albumItemBinding;
 
-        public AlbumViewHolder(AlbumItemBinding albumItemBinding) {
+        public AlbumViewHolder(AlbumItemBinding albumItemBinding, RecyclerViewInterface recyclerViewInterface) {
             super(albumItemBinding.getRoot());
             this.albumItemBinding = albumItemBinding;
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (recyclerViewInterface != null) {
+                        int position = getAdapterPosition();
+
+                        if (position != RecyclerView.NO_POSITION) {
+                            recyclerViewInterface.onItemClick(position);
+                        }
+                    }
+                }
+            });
         }
     }
+
 }
