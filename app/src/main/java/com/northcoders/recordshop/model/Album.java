6ad3a7b91e7dac0c24aacff1d42a122733466373
qiuqiu <qiujuanwang87@gmail.com>
2024-09-23 11:46:11 +0100

    refactor: change releaseYear to string in Album class

--- app/src/main/java/com/northcoders/recordshop/model/Album.java
+++ app/src/main/java/com/northcoders/recordshop/model/Album.java
@@ -1,8 +1,12 @@
 package com.northcoders.recordshop.model;
 
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
-import androidx.databinding.InverseMethod;
 import androidx.databinding.library.baseAdapters.BR;
 
 import com.google.gson.annotations.SerializedName;
@@ -10,35 +14,44 @@ import com.google.gson.annotations.SerializedName;
 public class Album extends BaseObservable {
 
     @SerializedName("id")
-    private Long id;
-
+    private long id;
+    @SerializedName("name")
+    private String name;
     @SerializedName("artist")
     private String artist;
-
     @SerializedName("releaseYear")
-    private int releaseYear;
-
+    private String releaseYear;
     @SerializedName("genre")
     private String genre;
 
-    @SerializedName("name")
-    private String name;
 
-    @SerializedName("imageUrl")
-    private String imageUrl;
-
-    public Album() {
-    }
-
-    public Album(String artist, String genre, Long id, String name, int releaseYear, String imageUrl) {
-        this.artist = artist;
-        this.genre = genre;
+    public Album(long id, String name, String artist, String releaseYear, String genre) {
         this.id = id;
         this.name = name;
+        this.artist = artist;
         this.releaseYear = releaseYear;
-        this.imageUrl = imageUrl;
+        this.genre = genre;
+        notifyPropertyChanged(BR.genre);
     }
 
+    @Bindable
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+        notifyPropertyChanged(BR.id);
+    }
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
     @Bindable
     public String getArtist() {
         return artist;
@@ -47,8 +60,17 @@ public class Album extends BaseObservable {
     public void setArtist(String artist) {
         this.artist = artist;
         notifyPropertyChanged(BR.artist);
+
+    }
+    @Bindable
+    public String getReleaseYear() {
+        return releaseYear;
     }
 
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+        notifyPropertyChanged(BR.releaseYear);
+    }
     @Bindable
     public String getGenre() {
         return genre;
@@ -57,47 +79,6 @@ public class Album extends BaseObservable {
     public void setGenre(String genre) {
         this.genre = genre;
         notifyPropertyChanged(BR.genre);
-    }
-
-    @Bindable
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-        notifyPropertyChanged(BR.id);
-    }
-
-    @Bindable
-    public String getName() {
-        return name;
-    }
-
-    @Bindable
-    public void setName(String name) {
-        this.name = name;
-        notifyPropertyChanged(BR.name);
-    }
-
-    @Bindable
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
-        notifyPropertyChanged(BR.releaseYear);
-    }
-
-    @Bindable
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-        notifyPropertyChanged(BR.imageUrl);
     }
 
     @InverseMethod("convertIntToString")
