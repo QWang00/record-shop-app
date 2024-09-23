7a1225e653efcf8ea38f59f994f25027f622fc81
qiuqiu <qiujuanwang87@gmail.com>
2024-09-23 11:56:30 +0100

    refactor: change button name in MainActivityClickHandler

--- app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivityClickHandler.java
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivityClickHandler.java
@@ -1,19 +1,22 @@
 package com.northcoders.recordshop.ui.mainactivity;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 
-import com.northcoders.recordshop.ui.addalbum.AddNewAlbumActivity;
+import com.northcoders.recordshop.ui.addAlbum.AddNewAlbumActivity;
 
 public class MainActivityClickHandler {
+
     private Context context;
 
     public MainActivityClickHandler(Context context) {
         this.context = context;
     }
-    public void onFABClicked(View view){
-        Intent intent = new Intent(view.getContext(), AddNewAlbumActivity.class);
-        context.startActivity(intent);
+
+    public void onAddNewAlbumPageBtnClicked(View view) {
+        Intent i = new Intent(view.getContext(), AddNewAlbumActivity.class);
+        context.startActivity(i);
     }
 }
