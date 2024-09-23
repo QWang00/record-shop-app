87ae7266f22ded47966f72a6741726620badef7a
qiuqiu <qiujuanwang87@gmail.com>
2024-09-23 11:54:21 +0100

    fix: change baseUrl to exclude "albums" in RetrofitInstance

--- app/src/main/java/com/northcoders/recordshop/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/recordshop/service/RetrofitInstance.java
@@ -5,22 +5,36 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
-    private final static String BASE_URL = "http://Record-shop-env.eba-zvfsktf7.eu-west-2.elasticbeanstalk.com/albums/";
+    private static Retrofit imageRetrofit = null;
+    private static final String BASE_URL = "http://RecordShop-env.eba-qatztzb9.eu-west-2.elasticbeanstalk.com/";
+    private static final String ITUNES_BASE_URL = "https://itunes.apple.com/";
 
-    public static AlbumApiService getService(){
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-        if(retrofit == null){
+    public static AlbumApiService getService() {
+
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
-                    .client(client)
+                    .client(getOKHttpClient())
                     .build();
-
         }
         return retrofit.create(AlbumApiService.class);
+    }
+}
+
+    private static OkHttpClient getOKHttpClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        return new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
     }
 }
